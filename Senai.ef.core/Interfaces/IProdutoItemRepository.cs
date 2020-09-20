6817e23ab5313066ec49901cf9a7ba69d3621a3e
Felipe Vidal <contatofelipevidalmendes@gmail.com>
2020-09-20 17:34:01 -0300

    mais uma class adicionada

---
+++ Senai.ef.core/Interfaces/IProdutoItemRepository.cs
@@ -0,0 +1,23 @@
+﻿using Senai.ef.core.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.ef.core.Interfaces
+{
+    interface IProdutoItemRepository
+    {
+        /// <summary>
+        /// Lista todos os pedidos
+        /// </summary>
+        /// <returns>Retorna uma lista de pedidos</returns>
+        List<Pedido> Listar();
+
+        /// <summary>
+        /// Adiciona um novo pedido
+        /// </summary>
+        /// <returns>Objeto Pedido</returns>
+        Pedido Adicionar(List<ProdutoItem> produtoItens);
+    }
+}
